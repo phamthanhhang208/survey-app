0ab20e5f436826eeb2605da0d8f1806f077b095f
minhdl <longminhdo2000@gmail.com>
2022-07-17 03:07:33 +0700

    done dnd frontend without api call

--- client/src/components/QuestionListView/QuestionListView.tsx
+++ client/src/components/QuestionListView/QuestionListView.tsx
@@ -1,16 +1,52 @@
 import QuestionViewCard from '@/components/QuestionViewCard/QuestionViewCard';
+import { FunctionComponent, useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import './QuestionListView.scss';
 
-const QuestionListView = (props: any) => {
-  const { questions } = props;
+const QuestionListView: FunctionComponent<any> = ({ questions, formId }) => {
+  const [questionOrder, setQuestionOrder] = useState(questions);
+
+  const handleOnDragEnd = (result: any) => {
+    // console.log(result);
+    if (!result.destination) {
+      return;
+    }
+
+    const items = Array.from(questionOrder);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setQuestionOrder(items);
+  };
+
   return (
-    <div className='question-list-view'>
-      {questions.map((q: any) => {
-        return (
-          <QuestionViewCard question={q} key={q._id} formId={props.formId} />
-        );
-      })}
-    </div>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId='cards'>
+        {(provided) => (
+          <div
+            className='question-list-view'
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {questionOrder.map((q: any, index: number) => {
+              return (
+                <Draggable key={q._id} draggableId={q._id} index={index}>
+                  {(provided) => (
+                    <QuestionViewCard
+                      question={q}
+                      key={q._id}
+                      formId={formId}
+                      provided={provided}
+                    />
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
