b40f570a2c0cb1beceb503553ae1af80f08e0574
phamthanhhang208 <phamthanhhang.208@gmail.com>
2022-08-01 21:37:10 +0700

    added share submenu

--- client/src/layouts/EditLayout/EditLayout.tsx
+++ client/src/layouts/EditLayout/EditLayout.tsx
@@ -1,36 +1,39 @@
-import { Layout } from 'antd';
-import { FunctionComponent } from 'react';
-import { NavLink, useParams } from 'react-router-dom';
-import './EditLayout.scss';
+import { Layout } from "antd";
+import { FunctionComponent } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import "./EditLayout.scss";
 
 interface EditLayoutProps {
-  children: any;
+	children: any;
 }
 
 const { Content } = Layout;
 
 const EditLayout: FunctionComponent<EditLayoutProps> = ({ children }) => {
-  const params = useParams();
+	const params = useParams();
 
-  return (
-    <div className='edit-layout'>
-      <div className='edit-layout-header'>
-        <NavLink className='edit-items' to={`/forms/${params.id}/edit`}>
-          <span>Questions</span>
-        </NavLink>
-        <NavLink
-          className='edit-items'
-          to={`/forms/${params.id}/edit-response`}
-        >
-          <span>Response</span>
-        </NavLink>
-        <NavLink className='edit-items' to={`/forms/${params.id}/edit-setting`}>
-          <span>Setting</span>
-        </NavLink>
-      </div>
-      <Content className='edit-layout-content'>{children}</Content>
-    </div>
-  );
+	return (
+		<div className="edit-layout">
+			<div className="edit-layout-header">
+				<NavLink className="edit-items" to={`/forms/${params.id}/edit`}>
+					<span>Questions</span>
+				</NavLink>
+				<NavLink
+					className="edit-items"
+					to={`/forms/${params.id}/edit-response`}
+				>
+					<span>Response</span>
+				</NavLink>
+				<NavLink className="edit-items" to={`/forms/${params.id}/edit-setting`}>
+					<span>Setting</span>
+				</NavLink>
+				<div className="edit-items">
+					<span>Share</span>
+				</div>
+			</div>
+			<Content className="edit-layout-content">{children}</Content>
+		</div>
+	);
 };
 
 export default EditLayout;
