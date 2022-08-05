9d428e77a700bd4656f0735df1b349b67385b343
phamthanhhang208 <phamthanhhang.208@gmail.com>
2022-08-06 01:47:41 +0700

    resolve merge conflict

--- client/src/components/Chart/ListDisplay.tsx
+++ client/src/components/Chart/ListDisplay.tsx
@@ -1,34 +1,31 @@
-import { Table } from 'antd';
-import { useId } from 'react';
+import { Table } from "antd";
 
 const ListDisplay = (props: any) => {
-  const { dataset } = props;
-  console.log(dataset);
-  const id = useId();
+	const { dataset } = props;
 
-  const columns = [
-    {
-      title: 'Content',
-      dataIndex: 'content',
-      key: 'content',
-    },
-    {
-      title: 'Count',
-      dataIndex: 'count',
-      key: 'count',
-      sorter: (a: any, b: any) => a.count - b.count,
-      width: 120,
-    },
-  ];
+	const columns = [
+		{
+			title: "Content",
+			dataIndex: "content",
+			key: "content",
+		},
+		{
+			title: "Count",
+			dataIndex: "count",
+			key: "count",
+			sorter: (a: any, b: any) => a.count - b.count,
+			width: 120,
+		},
+	];
 
-  return (
-    <Table
-      rowKey={() => id}
-      dataSource={dataset}
-      columns={columns}
-      pagination={{ pageSize: 3 }}
-    />
-  );
+	return (
+		<Table
+			rowKey={(item) => item.content}
+			dataSource={dataset}
+			columns={columns}
+			pagination={{ pageSize: 3 }}
+		/>
+	);
 };
 
 export default ListDisplay;
--- client/src/pages/EditSettingPage/EditSettingPage.tsx
+++ client/src/pages/EditSettingPage/EditSettingPage.tsx
@@ -1,83 +1,81 @@
-import React from 'react';
-import { Card, Form, Switch, Spin, Skeleton, Divider } from 'antd';
-import { useGetForm, useUpdateForm } from '@/hooks/form.hook';
-import { useParams } from 'react-router-dom';
-import './EditSettingPage.scss';
+import React from "react";
+import { Card, Form, Switch, Spin, Skeleton, Divider } from "antd";
+import { useGetForm, useUpdateForm } from "@/hooks/form.hook";
+import { useParams } from "react-router-dom";
+import "./EditSettingPage.scss";
 
 const EditSettingPage: React.FC = () => {
-  const { id } = useParams();
-  const [form] = Form.useForm();
+	const { id } = useParams();
+	const [form] = Form.useForm();
 
-  const { data: formDetail, isLoading } = useGetForm();
-  const { mutate: updateFormSetting } = useUpdateForm();
+	const { data: formDetail, isLoading } = useGetForm();
+	const { mutate: updateFormSetting } = useUpdateForm();
 
-  const handleOnChange = (values: any) => {
-    updateFormSetting({ id, values });
-  };
+	const handleOnChange = (values: any) => {
+		updateFormSetting({ id, values });
+	};
 
-  return (
-    <Card className='edit-setting-page'>
-      <h1>Settings</h1>
-      <Divider style={{ marginTop: 12, marginBottom: 24 }} />
-      {isLoading ? (
-        <Spin size='large' />
-      ) : (
-        <Form
-          layout='horizontal'
-          labelAlign='left'
-          colon={false}
-          labelCol={{ span: 22 }}
-          wrapperCol={{ span: 1 }}
-          form={form}
-          onValuesChange={handleOnChange}
-          initialValues={{
-            isAcceptResponse: formDetail?.isAcceptResponse,
-            isAllowAnonymous: formDetail?.isAllowAnonymous,
-          }}
-          className={'setting-form'}
-        >
-          <Form.Item
-            name='isAcceptResponse'
-            label={
-              <div>
-                <p style={{ fontSize: 16, marginBottom: 0 }}>
-                  Accept response:
-                </p>
-                <p
-                  style={{ fontStyle: 'italic', opacity: 0.8, marginBottom: 0 }}
-                >
-                  Descriptiuon
-                </p>
-              </div>
-            }
-            valuePropName='checked'
-            style={{ height: 64, marginBottom: 10 }}
-          >
-            <Switch />
-          </Form.Item>
-          <Form.Item
-            name='isAllowAnonymous'
-            label={
-              <div>
-                <p style={{ fontSize: 16, marginBottom: 0 }}>
-                  Allow anonymous respondents:
-                </p>
-                <p
-                  style={{ fontStyle: 'italic', opacity: 0.8, marginBottom: 0 }}
-                >
-                  Descriptiuon
-                </p>
-              </div>
-            }
-            valuePropName='checked'
-            style={{ marginBottom: 10 }}
-          >
-            <Switch />
-          </Form.Item>
-        </Form>
-      )}
-    </Card>
-  );
+	return (
+		<Card className="edit-setting-page">
+			<h1>Settings</h1>
+			<Divider style={{ marginTop: 12, marginBottom: 24 }} />
+			{isLoading ? (
+				<Spin size="large" />
+			) : (
+				<Form
+					layout="horizontal"
+					labelAlign="left"
+					colon={false}
+					labelCol={{ span: 22 }}
+					wrapperCol={{ span: 1 }}
+					form={form}
+					onValuesChange={handleOnChange}
+					initialValues={{
+						isAcceptResponse: formDetail?.isAcceptResponse,
+						isAllowAnonymous: formDetail?.isAllowAnonymous,
+					}}
+					className={"setting-form"}
+				>
+					<Form.Item
+						name="isAcceptResponse"
+						label={
+							<div>
+								<p style={{ fontSize: 16, marginBottom: 0 }}>Accept response</p>
+								<p
+									style={{ fontStyle: "italic", opacity: 0.8, marginBottom: 0 }}
+								>
+									Form is currently accepting responses
+								</p>
+							</div>
+						}
+						valuePropName="checked"
+						style={{ height: 64, marginBottom: 10 }}
+					>
+						<Switch />
+					</Form.Item>
+					<Form.Item
+						name="isAllowAnonymous"
+						label={
+							<div>
+								<p style={{ fontSize: 16, marginBottom: 0 }}>
+									Allow anonymous respondents
+								</p>
+								<p
+									style={{ fontStyle: "italic", opacity: 0.8, marginBottom: 0 }}
+								>
+									Form does not collect user email
+								</p>
+							</div>
+						}
+						valuePropName="checked"
+						style={{ marginBottom: 10 }}
+					>
+						<Switch />
+					</Form.Item>
+				</Form>
+			)}
+		</Card>
+	);
 };
 
 export default EditSettingPage;
